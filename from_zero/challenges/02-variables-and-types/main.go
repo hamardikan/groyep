9e3ceac8603dd05bfeaca7e7b5a6f5db8879bbd4
hamardikan <a.hamardikan@gmail.com>
2026-02-20 15:44:50 +0700

    (ch02: var and types)

---
+++ from_zero/challenges/02-variables-and-types/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "Gopher"
+	age := 10
+	height := 1.75
+	isAwesome := true
+
+	fmt.Printf("Name: %s\n", name)
+	fmt.Println("Age:", age)
+	fmt.Println("Height:", height)
+	fmt.Println("IsAwesome:", isAwesome)
+}
